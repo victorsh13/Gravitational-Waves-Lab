09fc177593fcca28b66201d41f4ce27b2f55f438
victorsh13 <victorserranoherreros@gmail.com>
2026-05-14 16:51:11 +0200

    CNN ispection

--- cbc_pe/src/models/network.py
+++ cbc_pe/src/models/network.py
@@ -224,3 +224,98 @@ class SimpleCNN_v2(nn.Module):
             return y_pred, embedding
 
         return y_pred
+
+
+class SimpleCNN_Pool(nn.Module):
+    
+    def __init__(
+            self, 
+            n_detectors=3,
+            n_outputs=3,
+            embedding_dim=32,
+            dropout_conv=0.05,
+            dropout_dense=0.1,
+            ):
+        
+        super().__init__()
+
+        # 1D-Conv Block
+        self.block1 = ConvBlock( 
+            in_channels=n_detectors,
+            out_channels=16,
+            kernel_size=16,
+            stride=2,
+            dropout=dropout_conv,
+        )
+        
+        # 1D-Conv Block
+        self.block2 = ConvBlock(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=16,
+            stride=2,
+            dropout=dropout_conv,
+        )
+
+        self.block3 = ConvBlock(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=16,
+            stride=2,
+            dropout=dropout_conv,
+        )
+
+        self.block4 = ConvBlock(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=16,
+            stride=2,
+            dropout=dropout_conv,
+        )
+
+        # Global Poolingg
+        self.pool = nn.AdaptiveAvgPool1d(8)
+
+        self.embedding_layer = nn.Sequential(
+            nn.Linear(128 * 8, 512),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Dropout(p=dropout_dense),
+
+            nn.Linear(512, 256),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Dropout(p=dropout_dense),
+            
+            nn.Linear(256, embedding_dim),
+        )
+
+        # Linear layer
+        self.head = nn.Linear(embedding_dim, n_outputs)
+
+
+    # ENCODER
+    def encode(self, x):    # (batch, channels, time) = (batch, 3, 16384)
+        x = self.block1(x)  # (b, 16, ~8192)
+        x = self.block2(x)  # (b, 32, ~4096)
+        x = self.block3(x)  # (b, 64, ~2048)
+        x = self.block4(x)  # (b, 128, ~1024)
+
+        x = self.pool(x)    # (b, 128, 8)
+        x = x.flatten(start_dim=1)  # (b, 128*8) = (b, 1024)
+
+        return x
+
+    # EMBEDDING
+    def embed(self, x):
+        features = self.encode(x)  # (b, 128)
+        embedding = self.embedding_layer(features)  # (b, emb_dim)
+        return embedding
+
+    # HEAD
+    def forward(self, x, return_embedding=False):
+        embedding = self.embed(x)
+        y_pred = self.head(embedding) #(b, 3)
+
+        if return_embedding:
+            return y_pred, embedding
+
+        return y_pred
